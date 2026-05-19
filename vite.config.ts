5e65e761fc1b5a5a794ce664b172c9d194945b47
Hasni <h.hasni@mytek.tn>
2026-05-19 14:21:10 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "https://hamza-hasnni.github.io/portfolio/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
